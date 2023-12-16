1fcc3fb931fa2075b58d8c7a003f63b5bf4c5455
sudb92 <bsudarsan92@gmail.com>
2023-12-15 21:44:32 -0500

    Add plotter for comparisons

--- reference/plot.C
+++ reference/plot.C
@@ -14,6 +14,13 @@
     c->cd(2);
     tt->Draw("fp.ic.de:tof.tac_xfpe1>>h_goddessSort(800,-200,600,800,0,1200)","","colz");
 
-    c->cd(2);
-    tt->Draw("(ts.timestamp-xtals.timestamp)>>dt_gret_s800(800,-200,600)","xtals.timestamp","colz");
+    auto c3 = c->cd(3);
+    c3->SetLogy();
+    tt_ref->Draw("(fts-gretinacalc.fhits.ftimestamp)>>dt_gts800_ref(400,-200,600)","gretinacalc.fhits.ftimestamp","goff");
+    TH1* h1 = (TH1*)(gDirectory->Get("dt_gts800_ref"));
+    h1->SetLineColor(kRed);
+    h1->Draw("histo");
+    h1->SetTitle("red: dt(s800,gt) ref, blue: dt(s800,gt) goddessSort");
+    tt->Draw("(ts.timestamp-xtals.timestamp)>>dt_gts800_goddessSort(400,-200,600)","xtals.timestamp","histosame");
+    c->SaveAs("test149_15Dec23.root");
 }
